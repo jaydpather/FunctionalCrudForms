439cb94e1b5769cd106b59f9d15235cfd8f614b1
Jayd <jaydpather@gmail.com>
2020-05-25 04:32:06 +0200

    able to show alerts again

--- Fable/UI/UI.fs
+++ Fable/UI/UI.fs
@@ -1,22 +1,22 @@
 module UI
 
-open System
+// open System
 open Fable.Core
-open Fable.Core.JS
-open Fable.Core.JsInterop
+// open Fable.Core.JS
+// open Fable.Core.JsInterop
 open Fable.Import
 open Fable.PowerPack
 open Fable.PowerPack.Fetch
-open Fable.PowerPack.Result
+// open Fable.PowerPack.Result
 open Thoth.Json
 open Browser.Dom
 open Browser.Types
-open Fable.Core.Util
+//open Fable.Core.Util
 open Model
 open Fable.PowerPack.PromiseImpl
 
-// [<Emit("alert('$0')")>]
-// let alert msg = jsNative
+[<Emit("alert('$0')")>]
+let alert msg = jsNative
 
 let private getElementByIdAbstract (window:Window) id = 
     unbox window.document.getElementById id
@@ -34,24 +34,10 @@ let getInputElementById (id:string):Browser.Types.HTMLInputElement =
 //let btnMain = getButtonElementById "btnMain" 
 //btnMain.onclick <- someFunc
 
-let submitForm = 
-    let txtName = getInputElementById("txtName")
-    let postData = { Name = txtName.value }
+// let submitForm = 
+//     let txtName = getInputElementById("txtName")
+//     let postData = { Name = txtName.value }
+//     0
 
-    let requestProps =
-        [ RequestProperties.Method HttpMethod.POST
-        ; requestHeaders [ContentType "application/json"]
-        ; RequestProperties.Body (unbox (toJson postData))
-        ]
-        //Encode.Auto.toString(1, postData)
-        //toJson    
-        //Fable.Core.JS.JSON.stringify
-
-    promise {
-        let! response = fetch "http://localhost:5000/employee/create" requestProps
-        let responseText = response.text()
-        Browser.console.log responseText
-    } |> ignore
-
-//alert("hello");
+alert("hello");
 printfn "page loaded"
