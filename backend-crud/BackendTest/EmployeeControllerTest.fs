5aff455bd07293afa5583208633c2453c22b3712
Jayd <jaydpather@gmail.com>
2020-09-05 15:06:21 +0200

    initial UT setup

---
+++ backend-crud/BackendTest/EmployeeControllerTest.fs
@@ -0,0 +1,12 @@
+﻿namespace RebelSoftware.UnitTests
+
+open NUnit.Framework
+
+module ControllerTests =
+    [<TestFixture>]
+    type TestClass () =
+
+        [<Test>]
+        member this.TestMethodPassing() =
+            Assert.IsTrue(true)
+    
