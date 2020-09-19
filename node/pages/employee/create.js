1b89a4ef0a9d34f1ae993a269953346c8e9097e1
Third Eye Software <jkp.thirdeyesoftware@gmail.com>
2020-09-19 16:56:20 +0200

    Node UTs - injectability complete (#56)
    * extracted into Controller, but not Logic
    
    * validator and postToServerFn are injectable
    
    * removing dead code from Component
    
    * component no longer initializes its own state
    
    * extracted EmployeeLogicService
    
    Co-authored-by: Jayd <jaydpather@gmail.com>

--- node/pages/employee/create.js
+++ node/pages/employee/create.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
 import Layout from '../../components/MyLayout';
 import ContactInfo from '../../components/ContactInfo';
+import EmployeeController from '../../controllers/EmployeeController';
+import { getEmployeeValidator } from '../../fable-include/Validation'
+import EmployeeLogicService from '../../logic/EmployeeLogicService';
+
+const axios = require('axios');
 
 export default class extends Component {
+
+  //todo: make postToServer into a reusable method w/ a param for URL
+  postToServer = async (employee) => {
+    let response = await axios.post("http://localhost:7000/employee/create", JSON.stringify(employee));
+    return response;
+  }
+
   render () {
     //todo: consistent tab width
+    let validator = getEmployeeValidator();
+    let employeeLogicService = new EmployeeLogicService();
+    let validateAndSubmitFn = employeeLogicService.validateAndSubmit(validator.ValidateEmployee, this.postToServer);
+    let employeeController = new EmployeeController(validateAndSubmitFn);
+    
     return (
       <Layout>
-        <ContactInfo />
+        <ContactInfo employeeController = {employeeController} />
       </Layout>
     )
   }
