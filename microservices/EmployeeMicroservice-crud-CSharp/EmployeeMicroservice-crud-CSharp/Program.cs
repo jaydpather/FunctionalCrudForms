9b74f8741dda3f3e7ebdf841360844e02927e3db
Jayd <jaydpather@gmail.com>
2020-09-27 09:42:57 -0500

    creating console app for CSharp microservice

---
+++ microservices/EmployeeMicroservice-crud-CSharp/EmployeeMicroservice-crud-CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeMicroservice_crud_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
