82884776d0985e8f5620cae14f4a1972c2082825
Jayd <jaydpather@gmail.com>
2020-09-27 16:45:39 -0500

    HttpService class, compiles

---
+++ backend-crud-CSharp/backend-crud-CSharp/HttpService.cs
@@ -0,0 +1,31 @@
+using System.IO;
+using Microsoft.AspNetCore.Http;
+
+namespace backend_crud_CSharp
+{
+    public class HttpService
+    {
+        private HttpContext _httpContext;
+        public HttpService(HttpContext context)
+        {
+            _httpContext = context;
+        }
+
+        public async void WriteHttpResponse(string response)
+        {
+            _httpContext.Response.Headers["Access-Control-Allow-Origin"] = new string[] {"*"}; //Microsoft.Extensions.Primitives.StringValues("*");
+            await _httpContext.Response.WriteAsync(response);
+        }
+
+        public string ReadRequestBody()
+        {
+            using(var reader = new StreamReader(_httpContext.Request.Body))
+            {
+                var bodyTask = reader.ReadToEndAsync();
+                bodyTask.Wait();
+                var body = bodyTask.Result;
+                return body;
+            }
+        }
+    }
+}
