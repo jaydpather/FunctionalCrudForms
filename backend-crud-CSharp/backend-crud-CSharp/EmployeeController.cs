d25283c40d175cfcd4eade352d61f3199a59b2d1
Jayd <jaydpather@gmail.com>
2020-09-28 05:16:46 -0500

    EmployeeController.Create, compiles

--- backend-crud-CSharp/backend-crud-CSharp/EmployeeController.cs
+++ backend-crud-CSharp/backend-crud-CSharp/EmployeeController.cs
@@ -1,3 +1,5 @@
+using System;
+
 using RebelSoftware.LoggingService;
 using RebelSoftware.MessageQueueService;
 using RebelSoftware.SerializationService;
@@ -19,6 +21,42 @@ namespace backend_crud_CSharp
             _serializationService = serializationService;
             _employeeValidator = employeeValidator;
             _httpService = httpService;
+        }
+
+        private string GetResponseString(string requestJsonString, Model.OperationResult operationResult)
+        {
+            string retVal = null;
+
+            if(operationResult.ValidationResult == Model.ValidationResults.Success)
+            {
+                retVal = _messageQueuer.WriteMessageAndGetResponse.Invoke(requestJsonString);
+            }
+            else
+            {
+                var objOperationResult = (object)operationResult;
+                retVal = _serializationService.SerializeToJson.Invoke(objOperationResult);
+            }
+            
+            return retVal;
+        }
+
+        public void Create()
+        {
+            try
+            {
+                var requestJsonString = _httpService.ReadRequestBody();
+                var employee = _serializationService.DeserializeFromJson.Invoke(requestJsonString);
+                var operationResult =  _employeeValidator.ValidateEmployee.Invoke(employee);
+                var responseStr = GetResponseString(requestJsonString, operationResult);
+                _httpService.WriteHttpResponse(responseStr);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogException.Invoke(ex);
+                var operationResult = new Model.OperationResult(Model.ValidationResults.UnknownError);
+                var responseStr = _serializationService.SerializeToJson.Invoke(operationResult);
+                _httpService.WriteHttpResponse(responseStr);
+            }
         }
     }
 }
