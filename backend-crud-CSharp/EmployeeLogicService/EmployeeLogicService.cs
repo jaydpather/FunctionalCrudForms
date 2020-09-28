e835b4ca04e8b3a131d030b239b83bcab683a67d
Jayd <jaydpather@gmail.com>
2020-09-28 12:00:45 -0500

    EmployeeLogicService

---
+++ backend-crud-CSharp/EmployeeLogicService/EmployeeLogicService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RebelSoftware.EmployeeLogic
+{
+    internal class EmployeeLogicService : IEmployeeLogicService
+    {
+        public Model.OperationResult ValidateEmployee(Model.Employee employee)
+        {
+            var firstNameResult = String.IsNullOrWhiteSpace(employee.FirstName) ? Model.ValidationResults.FirstNameBlank : Model.ValidationResults.Success;
+
+            var lastNameResult = String.IsNullOrWhiteSpace(employee.LastName) ? Model.ValidationResults.LastNameBlank : Model.ValidationResults.Success;
+
+            var finalResult = firstNameResult | lastNameResult;
+            var retVal = new Model.OperationResult(finalResult);
+            return retVal;
+        }
+    }
+}
