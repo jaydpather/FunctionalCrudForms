51b9198abf43d2439237df5d0574e390b57b8092
Jayd <jaydpather@gmail.com>
2020-10-02 20:43:19 -0500

    todo comments

--- backend-crud-Go/backendCrudGo.go
+++ backend-crud-Go/backendCrudGo.go
@@ -11,6 +11,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "this is the response")
 }
 
+//todo: 
+//	* listen on same port as existing back end
+//	* map URL of back end onto handler func
+//  * write hardcoded response (success message)
+
 func main(){
 	//fmt.Println("Hello, World!")
 	http.HandleFunc("/", handler)
