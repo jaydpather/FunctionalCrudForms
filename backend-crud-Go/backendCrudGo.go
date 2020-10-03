32b8eea73bd646b93933baa1a43ec96c77b5d04d
Jayd <jaydpather@gmail.com>
2020-10-02 21:45:00 -0500

    no more CORs error

--- backend-crud-Go/backendCrudGo.go
+++ backend-crud-Go/backendCrudGo.go
@@ -1,14 +1,31 @@
 package main 
 
 import (
-	"fmt"
-	"log"
+	//"fmt"
+	//"log"
 	"net/http"
+	"github.com/rs/cors"
 	//"./getString"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "this is the response")
+func employeeCreate(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("{\"hello\": \"world\"}"))
+
+	// enableCors(&w)
+	// if (*r).Method == "OPTIONS" {
+	// 	//fmt.Fprintf(w, "")
+	// } else {
+	// 	fmt.Fprintf(w, "{ \"ValidationResult\": 0 }")
+	// }	
+}
+
+func enableCors(w *http.ResponseWriter) {
+	//(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	//(*w).Header().Set("Access-Control-Allow-Methods", "POST")
+	(*w).Header().Set("Access-Control-Allow-Origin", "[\"*\"]")
+    (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
 //todo: 
@@ -18,7 +35,16 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main(){
 	//fmt.Println("Hello, World!")
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	//http.HandleFunc("/employee/create/", employeeCreate)
+	//log.Fatal(http.ListenAndServe(":7000", nil))
 	//fmt.Println(getString.GetHelloMessage())
+
+	mux := http.NewServeMux()
+    mux.HandleFunc("/employee/create", employeeCreate)
+
+    // cors.Default() setup the middleware with default options being
+    // all origins accepted with simple methods (GET, POST). See
+    // documentation below for more options.
+    handler := cors.Default().Handler(mux)
+    http.ListenAndServe(":7000", handler)
 }
